f2ecaa30828f9e25df56a9edfd655443145a75d9
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2025-05-23 23:00:57 +0600

    adding flag

--- react-world/react-world-tour/src/Countries.jsx
+++ react-world/react-world-tour/src/Countries.jsx
@@ -1,50 +1,63 @@
 import { useEffect, useState } from 'react';
 import Country from './Country/Country';
 
-
 const Countries = () => {
   const [countries, setcountries] = useState([]);
-  const [visitedCountries,setVisitedCountries]=useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
   console.log(countries);
   const [count, setCount] = useState(0);
   const handleChangeAmout = () => {
     const newAmount = count + 1;
     setCount(newAmount);
   };
-  const handleVisitedCountry=country=>{
+  const handleVisitedCountry = country => {
     console.log('add this to your visited country');
     console.log(country);
 
-    const newVisitCountry = [...visitedCountries,country];
+    const newVisitCountry = [...visitedCountries, country];
     setVisitedCountries(newVisitCountry);
-  }
+  };
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
       .then(data => setcountries(data));
   }, [count]);
+  const [VisitedFlag, setVisitedFlags] = useState([]);
+
+ const handleVisitedFlag = flags => {
+  const newVisitedFlags = [...VisitedFlag, flags]; 
+  setVisitedFlags(newVisitedFlags);
+}
+
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={() => handleChangeAmout()}>count</button>
-      <h3 className='text-2xl'>countries:{countries.length}</h3>
+      <h3 className="text-2xl">countries:{countries.length}</h3>
       <div>
-       <div>
-        <h5>
-          visited countries:{visitedCountries.length}
-        </h5>
-        <ul>{
-             visitedCountries.map(country => <li key={country.cca3} >{country.name.common}</li>)
+        {/* visited countries */}
+        <div>
+          <h5>visited countries:{visitedCountries.length}</h5>
+          <ul>
+            {visitedCountries.map(country => (
+              <li key={country.cca3}>{country.name.common}</li>
+            ))}
+          </ul>
+        </div>
+        <div className='flag-container w-50'>{
+          VisitedFlag.map(flag => <img src={flag}></img>)
           }
-      
-        </ul>
-       </div>
 
+        </div>
+        {/* display countries */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {countries.map(country => (
-          <Country  country={country}
-          handleVisitedCountry={handleVisitedCountry}></Country>
+          <Country
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlag={handleVisitedFlag}
+          ></Country>
         ))}
       </div>
     </div>
--- react-world/react-world-tour/src/Country/Country.jsx
+++ react-world/react-world-tour/src/Country/Country.jsx
@@ -4,9 +4,7 @@
 
 // // import { useState } from "react";
 
-
 // // import { useState } from "react";
-
 
 // // import './Country.css'
 // const Country = ({country}) => {
@@ -24,7 +22,7 @@
 //       <p>Population:{population}</p>
 //       <p>area:{area}</p>
 //       <p><small>code:{cca3}</small></p>
-    
+
 //       <button className='bg-red-800 px-2 py-2 border-4 border-red-400' onClick={handleVisited}>{visited ?'visited':'going'}</button>{
 //         visited ? 'i have visited this country.' :'i want to visit'
 //       }
@@ -33,35 +31,48 @@
 // };
 
 // export default Country;
-import { useState } from "react";
+import { useState } from 'react';
 
-const Country = ({ country,handleVisitedCountry }) => {
+const Country = ({ country, handleVisitedCountry, handleVisitedFlag }) => {
   const { name, flags, population, area, cca3 } = country;
   const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
-    setVisited(true); 
+    setVisited(true);
   };
-  
-// const passWithParams =
+
+  // const passWithParams =
 
   return (
-    <div  className={`border-gray-900 border-2 ${visited ? 'bg-lime-400 text-white ' : ''} `}>
+    <div
+      className={`border-gray-900 border-2 ${
+        visited ? 'bg-lime-400 text-white ' : ''
+      } `}
+    >
       <h3>Name: {name.common}</h3>
       <img src={flags.png} alt={`${name.common} flag`} />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
-      <p><small>Code: {cca3}</small></p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
 
-      <div  className= {`${visited ? 'bg-red-400 text-black px-4 py-2 ' : 'bg-green-400 text-white  px-4 py-2 cursor-pointer'}`} disabled={visited}
+      <div
+        className={`${
+          visited
+            ? 'bg-red-400 text-black px-4 py-2 '
+            : 'bg-green-400 text-white  px-4 py-2 cursor-pointer'
+        }`}
+        disabled={visited}
         onClick={handleVisited}
       >
-        <span >{visited ? 'Visited' : 'Going'}</span>
+        <span>{visited ? 'Visited' : 'Going'}</span>
       </div>
-    <br />
-      <button onClick={()=>
-  handleVisitedCountry(country)}>Mark Visited</button>
-
+      <br />
+      <button onClick={() => handleVisitedCountry(country)}>
+        Mark Visited
+      </button>
+<button onClick={()=>handleVisitedFlag(country.flags.png)}>Add Flag</button>
       <p className="mt-2">
         {visited ? 'I have visited this country.' : 'I want to visit.'}
       </p>
